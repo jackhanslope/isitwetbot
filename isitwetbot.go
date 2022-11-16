62a6466018b30292e05f1eb7fe6f1e07ca714927
Jack Hanslope <code@jackhanslope.com>
2022-11-16 18:04:06 +0000

    Add basic function which can send a message

--- isitwetbot.go
+++ isitwetbot.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
 
 func main() {
-	fmt.Println("Yes, it's probably wet")
+	var err error
+
+	message := "Yes, it's probably wet"
+	token := os.Getenv("TELEGRAM_TOKEN")
+	chat_id := os.Getenv("CHAT_ID")
+
+	if err = sendMessage(message, token, chat_id); err != nil {
+		log.Println(err)
+	}
+}
+
+func sendMessage(message string, token string, chat_id string) (err error) {
+	messageUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?", token)
+	v := url.Values{}
+	v.Set("chat_id", chat_id)
+	v.Set("text", message)
+	messageUrl += v.Encode()
+	_, err = http.Get(messageUrl)
+	return
 }
